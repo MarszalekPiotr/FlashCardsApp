629948b138a89f00afd9f78b02caf38b2b3943bc
MarszalekPiotr <marszalekpiotr183@gmail.com>
2026-06-08 18:35:03 +0200

    Get Me

---
+++ src/Application/Users/GetCurrent/GetUserByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+using Application.Abstractions.Authentication;
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Application.Users.DTO;
+using Application.Users.GetById;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+
+namespace Application.Users.GetCurrent;
+
+internal sealed class GetCurrentUserQueryHandler(IUserContext userContext,IUserReadRepository userReadRepository)
+    : IQueryHandler<GetCurrentUserQuery, UserResponse>
+{
+    public async Task<Result<UserResponse>> Handle(GetCurrentUserQuery   query, CancellationToken cancellationToken)
+    {
+        
+
+       UserReadModel user = await userReadRepository.GetById(userContext.UserId);
+
+        if (user is null)
+        {
+            return Result.Failure<UserResponse>(UserErrors.NotFound(userContext.UserId));
+        }
+
+        var userResponse = new UserResponse
+        {
+            Id = user.Id,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email.ToString()
+        };     
+
+        return userResponse;
+    }
+}
---
+++ src/Web.Api/Endpoints/Users/GetMe.cs
@@ -0,0 +1,27 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Users.GetCurrent;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Users;
+
+internal sealed class GetMe : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("users/me", async (
+
+            IQueryHandler<GetCurrentUserQuery, UserResponse> handler,
+            CancellationToken cancellationToken) =>
+        {
+            var query = new GetCurrentUserQuery();
+
+            Result<UserResponse> result = await handler.Handle(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .HasPermission(Permissions.UsersAccess)
+        .WithTags(Tags.Users);
+    }
+}
